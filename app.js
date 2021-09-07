ed9e8ee9751815be800894fe47c3fcde1ba638e8
Vinicius Elias <viniciuselias79@gmail.com>
2021-09-07 16:03:39 -0300

    Update change claro escuro

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 
 const chk = document.getElementById('chk');
 const theme = document.querySelector('#theme-link');
+
+if (localStorage.getItem('tema') == 'claro') {
+    theme.href = "light-theme.css";
+    localStorage.setItem('tema', 'claro');
+} else {
+    theme.href = "dark-theme.css";
+    localStorage.setItem('tema', 'escuro');
+}
+
 chk.addEventListener('change', () => {
 	if (theme.getAttribute("href") == "light-theme.css") {
 		theme.href = "dark-theme.css"
+		localStorage.setItem('tema', 'escuro');
 	} else {
 		theme.href = "light-theme.css"
+		localStorage.setItem('tema', 'claro');
 	}
 });
--- sobre/sobre.js
+++ sobre/sobre.js
@@ -2,10 +2,19 @@
 
 const chk = document.getElementById('chk');
 const theme = document.querySelector('#theme-link');
+
+if (localStorage.getItem('tema') == 'escuro') {
+    theme.href = "sobre-dark-theme.css";
+    localStorage.setItem('tema', 'escuro');
+} else {
+    theme.href = "sobre-light-theme.css";
+    localStorage.setItem('tema', 'claro');
+}
+
 chk.addEventListener('change', () => {
 	if (theme.getAttribute("href") == "sobre-light-theme.css") {
-		theme.href = "sobre-dark-theme.css"
+		theme.href = "sobre-dark-theme.css";
 	} else {
-		theme.href = "sobre-light-theme.css"
+		theme.href = "sobre-light-theme.css";
 	}
 });
