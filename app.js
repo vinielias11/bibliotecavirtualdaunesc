6bd61f06b77367c3e77006929d0f6edf8ee293bb
Vinicius Elias <viniciuselias79@gmail.com>
2021-09-16 22:26:07 -0300

    API

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ chk.addEventListener('change', () => {
 });
 
 function bookSearch () {
-    var item, titulo, autor, publisher, bookLink, bookImg;
+    var item, titulo, autor, publisher, bookIsbn, bookLink, bookImg, 
+    placeholder = '<img src="https://via.placeholder.com/150">';
     var search = document.getElementById('pGeral').value;
 
     if (search == '') {
@@ -43,13 +44,23 @@ function bookSearch () {
 
         success: function (data) {
             for (i = 0; i < data.items.length; i++) {
-                results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
+                // results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
+                item = data.items[i];
+                titulo = item.volumeInfo.title;
+                autor = item.volumeInfo.author;
+                publisher = item.volumeInfo.publisher;
+                bookLink = item.volumeInfo.previewLink;
+                bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier;
+                bookImg = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeholder;
+                results.innerHTML += '<div style="margin-top: 4px;">' + formatOutput(bookImg, titulo, autor, publisher, bookLink)
             }
         },
         
         type: 'GET'
     })
 }
+
+
 
 function habilitaResultados () {
     var caixa = document.getElementById('pesquisaResultados');
